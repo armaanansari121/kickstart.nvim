ca3d71d2f4825edee1e73490f8f461d4424ec805
Armaan Ansari <armaanm2a@gmail.com>
2025-05-10 16:07:52 +0530

    Configured cairo_ls

--- init.lua
+++ init.lua
@@ -652,7 +652,9 @@ require('lazy').setup({
       --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
       local capabilities = require('blink.cmp').get_lsp_capabilities()
-
+      vim.lsp.config('cairo_ls', {
+        cmd = { 'scarb', 'cairo-language-server', '/C', '--node-ipc' },
+      })
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
